e5ac45481c35dc3b4761c0f10fa82b38b3da8885
fengxiaoqing <fy88fy@163.com>
2018-11-20 21:26:39 +0800

    hellopy.py

---
+++ homework/hellopy.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/20 21:24
+# @Author  : Feng Xiaoqing
+# @FileName: hellopy.py.py
+# @Software: PyCharm
+# @Function：
+#-----------------------------------
+print ('hello python')

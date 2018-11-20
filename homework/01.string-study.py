d30d013aa712563c309d43c2c0d8ebef11766f0c
fengxiaoqing <fy88fy@163.com>
2018-11-20 22:29:14 +0800

    练习1 字符串练习

--- homework/01.string-study.py
+++ homework/01.string-study.py
@@ -6,17 +6,18 @@
 # @Function：
 #----------------------------------- 
 
-
-# 练习1：
-# 将 “123” 转换成整数
-# 将 “9999999999999999999” 转换成长整数
-# 将 “3.1415926” 转换成一个浮点数
-# 将 123 转换成一个字符串
-# 现有以下字符串
-# 字符串1："   abc  deFGh&*ijkl opq mnrst((uvwxyz   "
-# 字符串2："   ABC#DEF  GH%IJ MNOPQ KLRS&&TUVWX(*&YZ   "
-# 使用字符串的各种方法转换成如下方式
-# ABCDEFGHIJKLMNOPQRSTUVWXYZzyxwvutsrqponmlkjihgfedcba
+'''
+练习1：
+将 “123” 转换成整数
+将 “9999999999999999999” 转换成长整数
+将 “3.1415926” 转换成一个浮点数
+将 123 转换成一个字符串
+现有以下字符串
+字符串1："   abc  deFGh&*ijkl opq mnrst((uvwxyz   "
+字符串2："   ABC#DEF  GH%IJ MNOPQ KLRS&&TUVWX(*&YZ   "
+使用字符串的各种方法转换成如下方式
+ABCDEFGHIJKLMNOPQRSTUVWXYZzyxwvutsrqponmlkjihgfedcba
+'''
 
 
 a=int("123")

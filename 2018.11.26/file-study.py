1925a9ef27c95f85b32de47d9235762dea843df0
fengxiaoqing <fy88fy@163.com>
2018-11-27 07:21:54 +0800

    file-study

---
+++ 2018.11.26/file-study.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/26 21:15
+# @Author  : Feng Xiaoqing
+# @FileName: file-study.py
+# @Software: PyCharm
+# @Function：
+#----------------------------------- 
+x=1
+y=2
+data=x if x>y else y;
+print data
---
+++ 2018.11.26/study1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/26 21:07
+# @Author  : Feng Xiaoqing
+# @FileName: study1.py
+# @Software: PyCharm
+# @Function：
+#----------------------------------- 
+with open('a',mode='r') as f:
+    # f.write('111111111')
+    # f.write('222222222')
+    # f.write('222222222\n')
+    # f.write('222222222\n')
+    # f.write('222222222\n')
+    # f.write('222222222\n')
+    print(f.readline(),)
+    # f.seek(0)
+    print(f.readline(),)

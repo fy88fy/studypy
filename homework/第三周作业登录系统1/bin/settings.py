31be0d41aa12d47b65093546624d9b01c2decf67
fengxiaoqing <fy88fy@163.com>
2018-12-12 08:42:00 +0800

    完成第三周作业登录系统class

---
+++ homework/第三周作业登录系统1/bin/settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+class userinfo(object):
+    a = 100
+    user1 = 'fengxiaoqing:123456'
+    user2 = 'fxq:12345678'
+    user3 = 'zhangsan:111222'
+    user4 = 'lisi:111222333'
+    print (user1)
+    print (user2)
+    print (user3)
+    print (user4)
